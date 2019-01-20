23e52eb42ca48a18afb65acad6eced903e61a4b3
sandeep <sandeep_k17@yahoo.com>
2019-01-20 16:12:07 +0100

    added pytest module

---
+++ pytest.py
@@ -0,0 +1,6 @@
+// Testing python modules
+import pytest
+import from prometheus_client import Counter
+c = Counter('my_requests_total', 'HTTP Failures', ['method', 'endpoint'])
+c.labels(method='get', endpoint='/').inc()
+c.labels(method='post', endpoint='/submit').inc()
